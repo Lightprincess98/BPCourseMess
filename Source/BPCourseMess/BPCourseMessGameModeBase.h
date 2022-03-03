489df6782801598137b99d7b910fcaabb1ce1eb0
Alexis Stahnke <alexis.stahnke@icloud.com>
2022-03-03 11:10:18 +0200

    Initial Commit
    My initial files that I worked with (Without starter content, please add starter content)

---
+++ Source/BPCourseMess/BPCourseMessGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BPCourseMessGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BPCOURSEMESS_API ABPCourseMessGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
